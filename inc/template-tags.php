c486cab26f02a3ba67d3686f256fad2eaf77ba25
Stacy Kvernmo <stacy@oddbird.net>
2017-05-24 22:45:42 -0500

    Filter the archive title

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -396,5 +396,27 @@ function yoga_get_hero() {
 			<!--<a href="#" class="hero-button" title="Click to see more">Click Me</a>-->
 		</div>
 	</section>
+
 <?php }
 
+/**
+ * Filter archive title?
+ *
+ * @return title
+ */
+function yoga_archive_title( $title ) {
+    if ( is_category() ) {
+        $title = single_cat_title( '', false );
+    } elseif ( is_tag() ) {
+        $title = single_tag_title( '', false );
+    } elseif ( is_author() ) {
+        $title = '<span class="vcard">' . get_the_author() . '</span>';
+    } elseif ( is_post_type_archive() ) {
+        $title = post_type_archive_title( '', false );
+    } elseif ( is_tax() ) {
+        $title = single_term_title( '', false );
+    }
+    return $title;
+}
+
+add_filter( 'get_the_archive_title', 'yoga_archive_title' );
