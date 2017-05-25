c09757f84e87ad41ed64070c7d613795ece76617
Stacy Kvernmo <stacy@oddbird.net>
2017-05-24 22:46:58 -0500

    may not use, but adding sidebar-filter here in case

---
+++ sidebar-filter.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * The sidebar containing thefilter widget area.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package yoga
+ */
+
+if ( ! is_active_sidebar( 'sidebar-archive' ) ) {
+	return;
+}
+?>
+
+
+<div class="secondary widget-area" role="complementary">
+	<?php	dynamic_sidebar( 'sidebar-archive' ); ?>
+</div><!-- .secondary -->
+
