518d7eddd33cb39e433593ad47e4276a9d027c1d
Stacy Kvernmo <stacy@oddbird.net>
2017-07-02 22:21:29 -0500

    clean up work

--- anspress/question-list-item.php
+++ anspress/question-list-item.php
@@ -16,40 +16,47 @@ $clearfix_class = array( 'ap-questions-item clearfix' );
 
 ?>
 <div id="question-<?php the_ID(); ?>" <?php post_class( $clearfix_class ); ?>>
-	<a class="ap-questions-hyperlink" itemprop="url" href="<?php the_permalink(); ?>" rel="bookmark" title="<?php the_title(); ?>">
+	<div class="ap-questions-inner">
+		<div class="ap-questions-summery">
 
-		<div class="ap-questions-inner">
-			<div class="ap-questions-summery">
-
-				<div class="ap-avatar ap-pull-left">
+			<div class="ap-avatar ap-pull-left">
+				<a href="<?php echo ap_user_link(); ?>">
 					<?php ap_author_avatar( ap_opt( 'avatar_size_list' ) ); ?>
-				</div>
+				</a>
+			</div>
 
-				<span class="ap-questions-title" itemprop="title">
+			<a class="ap-questions-hyperlink ap-questions-title" itemprop="url" href="<?php the_permalink(); ?>" rel="bookmark" title="<?php the_title(); ?>">
+				<span itemprop="title">
 					<?php ap_question_status(); ?>
 					<?php the_title(); ?>
 				</span>
+			</a>
 
-				<div class="ap-display-question-meta">
-					<?php // echo ap_question_metas() ?>
+			<div class="ap-display-question-meta">
+				<?php //echo ap_question_metas(); ?>
 
-					<div class="ap-list-counts">
-						<!-- Votes count -->
-						<?php if ( ! ap_opt( 'disable_voting_on_question' ) ) : ?>
-							<!--<span class="ap-questions-count ap-questions-vcount">
-								<span><?php ap_votes_net(); ?></span>
-								<?php _e( 'Votes', 'anspress-question-answer' ); ?>
-							</span>-->
-						<?php endif; ?>
+				<div class="ap-list-counts">
+					<!-- Votes count -->
+					<?php if ( ! ap_opt( 'disable_voting_on_question' ) ) : ?>
+						<!--<span class="ap-questions-count ap-questions-vcount">
+							<span><?php ap_votes_net(); ?></span>
+							<?php _e( 'Votes', 'anspress-question-answer' ); ?>
+						</span>-->
+					<?php endif; ?>
 
-						<!-- Answer Count -->
-						<div class="ap-questions-count ap-questions-acount">
-							<span><?php ap_answers_count(); ?></span>
-							<?php _e( 'Answers', 'anspress-question-answer' ); ?>
-						</div>
+					<!-- Answer Count -->
+					<div class="ap-questions-count ap-questions-acount">
+						<span><?php ap_answers_count(); ?></span>
+						<?php _e( 'Answers', 'anspress-question-answer' ); ?>
+					</div>
 
-					</div><!-- end ap-list-counts -->
-				</div><!-- end ap-display-question-meta -->
+				</div><!-- end ap-list-counts -->
+			</div><!-- end ap-display-question-meta -->
+
+			<div class="question-excerpt">
+				<?php the_excerpt(); ?>
+			</div>
+
 			</div><!-- end ap-questions-summery -->
 		</div><!-- end ap-questions-inner -->
 
