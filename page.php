d55568ba78adfba2fcb3e1986b4d3947396c99ad
Stacy Kvernmo <stacy@oddbird.net>
2017-07-02 22:20:25 -0500

    move sidebar

--- page.php
+++ page.php
@@ -24,17 +24,18 @@ get_header(); ?>
 					get_template_part( 'template-parts/content', 'page' );
 
 					// If comments are open or we have at least one comment, load up the comment template.
-					if ( comments_open() || get_comments_number() ) :
-						comments_template();
-					endif;
+					// if ( comments_open() || get_comments_number() ) :
+					// 	comments_template();
+					// endif;
 
 				endwhile; // End of the loop.
 				?>
 
+				<?php get_sidebar(); ?>
+
 			</main><!-- #main -->
 		</div><!-- .primary -->
 
-		<?php get_sidebar(); ?>
 
 	</div><!-- .wrap -->
 
