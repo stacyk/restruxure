f289eae4062eb8f29633a94873b3ca4c8e3c591d
Stacy Kvernmo <stacy@oddbird.net>
2017-07-10 19:32:43 -0500

    add home page, delete ask page

--- page-home.php
+++ page-home.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Template Name: ask
+ * Template Name: home
  *
  * This is the template that displays all pages by default.
  * Please note that this is the WordPress construct of pages
