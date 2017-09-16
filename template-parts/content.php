75eb6defbe73f66272999ceb46b5d4b4b840df4e
Stacy Kvernmo <stacy@oddbird.net>
2017-09-15 22:08:20 -0500

    add post image

--- template-parts/content.php
+++ template-parts/content.php
@@ -17,6 +17,11 @@
 		else :
 			the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
 		endif;
+
+		if ( has_post_thumbnail() ) :
+			the_post_thumbnail('post-hero');
+		endif;
+
 		if ( 'post' === get_post_type() ) : ?>
 		<div class="entry-meta">
 			<?php yoga_posted_on(); ?>
