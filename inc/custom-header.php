0836e0e8c43dcda49f9637ea257554a4ab14ff93
Stacy Kvernmo <stacy@oddbird.net>
2017-09-15 22:05:37 -0500

    header size

--- inc/custom-header.php
+++ inc/custom-header.php
@@ -24,8 +24,8 @@ function yoga_custom_header_setup() {
 	add_theme_support( 'custom-header', apply_filters( 'yoga_custom_header_args', array(
 		'default-image'          => '',
 		'default-text-color'     => '000000',
-		'width'                  => 1000,
-		'height'                 => 250,
+		'width'                  => 1400,
+		'height'                 => 325,
 		'flex-height'            => true,
 		'wp-head-callback'       => 'yoga_header_style',
 		'admin-head-callback'    => 'yoga_admin_header_style',
