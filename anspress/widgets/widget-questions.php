137b5a83e9424072bf25c77f23de573ba2dd5d92
Stacy Kvernmo <stacy@oddbird.net>
2018-01-05 19:56:04 -0600

    styles - general

---
+++ anspress/widgets/widget-questions.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * AnsPress questions widget template.
+ *
+ * @link https://anspress.io/anspress
+ * @since 2.0.1
+ * @author Rahul Aryan <support@anspress.io>
+ * @package AnsPress
+ */
+
+?>
+<div class="ap-questions-widget clearfix">
+	<?php if ( ap_have_questions() ) : ?>
+		<?php while ( ap_have_questions() ) : ap_the_question(); ?>
+			<div class="ap-question-item">
+				<a class="ap-question-title" href="<?php the_permalink() ?>"><?php the_title() ?></a>
+				<span class="ap-ans-count meta"><?php printf( _n( '1 Answer', '%d Answers', ap_get_answers_count(), 'anspress-question-answer' ), ap_get_answers_count() ) ?></span>
+			</div>
+		<?php endwhile; ?>
+	<?php else : ?>
+		<?php _e( 'No questions found.', 'anspress-question-answer' ); ?>
+	<?php endif; ?>
+</div>
+
+
