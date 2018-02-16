bb15f9a89908f8467e8c9b57b4cfd946a302207a
Stacy Kvernmo <stacy@oddbird.net>
2018-02-15 20:43:14 -0600

    simple sample custom form

---
+++ inc/custom-form.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Register a custom form by "sample_form" name in AnsPress.
+ *
+ * @return array
+ */
+function yoga_custom_form_in_ap() {
+  $form = array(
+    'fields' => array(
+      'input_field_1' => array( // This is a unique key for field.
+        'label' => __('Simple input field'),
+        'desc' => __('A simple description about this field.'),
+        'type' => 'input', // Default type is input.
+        'subtype' => 'text' // Default subtype is text.
+      ),
+      'email_field_2' => array(
+        'label' => __('Sample email field'),
+        'desc' => __('A simple description about this field.'),
+        'subtype' => 'email', // Default subtype is text.
+        'attr' => array(
+          'placeholder' => __('Enter your email'),
+          'class' => 'custom-css-class'
+        )
+      ),
+      'pages' => array(
+        'label' => __('Page field'),
+        'desc' => __('A sample field for selecting a page.'),
+        'type' => 'select',
+        'options' => 'posts',
+        'post_args' => array(
+          'post_type' => 'page'
+        )
+      ),
+      'editor' => array(
+        'label' => __('Sample editor field'),
+        'desc' => __('A TinyMce editor field.'),
+        'type' => 'editor'
+      )
+    )
+  );
+
+  return $form;
+}
+// ap_form_ followed by form name.
+add_filter('ap_form_sample_form', 'yoga_custom_form_in_ap');
